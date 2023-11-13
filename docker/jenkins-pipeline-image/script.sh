0442e1d8e5a4044cdede442e46e706c68000ff39
olawaleoyg <olawaleoyg@gmail.com>
2023-11-13 04:14:58 +0000

    initial deployment

---
+++ docker/jenkins-pipeline-image/script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+source .env
+docker ps --format "table {{.ID}}\t{{.Ports}}"
+docker build -t $IMAGE_NAME_VERSION . --no-cache
+docker images ls
+# docker run -d $IMAGE_NAME_VERSION
+docker run -d $IMAGE_NAME_VERSION
+docker ps
+echo "docker-build successful"
+# push image to dockerhub
+docker tag $IMAGE_NAME_VERSION $REPOSITORY_NAME
+docker push $REPOSITORY_NAME
+echo "push successful"

a601b54d94193ccec70e49f5524d4fb2869f8491
olawaleoyg <olawaleoyg@gmail.com>
2023-11-15 23:01:55 +0000

    delete ec2 update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,17 +1,17 @@
 
 
-/* create ec2 instance */
-  resource "aws_instance" "testJenkins" {
-  ami           = "ami-0eb260c4d5475b901"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-00be2de1445cb3a00"
-  # vpc_security_group_ids =""
-  key_name = "monitoring"
-  associate_public_ip_address = false
-  # volume_type = "ebs"
-  # volume_size = "8"
+# /* create ec2 instance */
+#   resource "aws_instance" "testJenkins" {
+#   ami           = "ami-0eb260c4d5475b901"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-00be2de1445cb3a00"
+#   # vpc_security_group_ids =""
+#   key_name = "monitoring"
+#   associate_public_ip_address = false
+#   # volume_type = "ebs"
+#   # volume_size = "8"
 
-  tags = {
-    Name = "NEW RUN"
-  }
-}
+#   tags = {
+#     Name = "NEW RUN"
+#   }
+# }
